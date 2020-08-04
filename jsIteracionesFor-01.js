650b9118cb733c5574c6deede0ebfb633c2634d3
davidchomczyk78 <68852078+davidchomczyk78@users.noreply.github.com>
2020-08-04 11:08:12 -0300

    Add files via upload
    archivo nuevo

---
+++ jsIteracionesFor-01.js
@@ -0,0 +1,7 @@
+function mostrar()
+{
+	var i;
+	for(i =1 ; i<=10; i++){
+		console.log(i);
+	}
+}
---
+++ jsIteracionesFor-02.js
@@ -0,0 +1,11 @@
+function mostrar()
+{
+
+	var i;
+	for(i =10 ; i>=1; i--){
+	if(i == 1){
+		break;
+	}
+		console.log(i);
+    }
+}
